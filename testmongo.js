34b759e4bdaff86ac632aa04395cd814c5680ab8
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-03-28 11:27:08 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<brennononeill>:<CMPS415>@bomdb.g5uygmr.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://brennononeill:CMPS415@bomdb.g5uygmr.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
